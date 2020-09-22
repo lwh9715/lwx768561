a35311ae923f56878ee568284926210066c87faf
lwx768561 <lwh9715@gmail.com>
2020-09-22 22:55:46 +0800

    合并spring项目

--- springboot/src/main/java/com/example/bean/Order.java
+++ springboot/src/main/java/com/example/bean/Order.java
@@ -8,4 +8,13 @@ public class Order {
     private String o_name;
     private String o_date;
     private String o_images;
+
+    public Order() {
+    }
+    public Order(Integer o_id, String o_name, String o_date, String o_images) {
+        this.o_id = o_id;
+        this.o_name = o_name;
+        this.o_date = o_date;
+        this.o_images = o_images;
+    }
 }
